a9ed7a6b33802b95ecc56b973a18fc12bd17bf00
christiansallen <christiansallen@gmail.com>
2019-11-11 16:33:44 -0700

    I think I finished MVP....

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,22 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.registers = [0]*8
+        self.ir = 0
+        self.pc = 0
+
+    def ram_read(self, index):
+        return self.ram[index]
+
+    def ram_write(self, index, value):
+        self.ram[index] = value
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +28,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +57,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +71,24 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        self.pc = 0
+        running = True
+        while running is True:
+            # print(self.ram)
+            self.ir = self.ram[self.pc]
+            operand_a = self.ram[self.pc+1]
+            operand_b = self.ram[self.pc+2]
+            # HLT
+            if self.ir == 0b00000001:
+                break
+            # LDI
+            elif self.ir == 0b10000010:
+                print(f'LDI:{operand_b}')
+                self.registers[operand_b-1] = operand_a
+                self.pc += 3
+            # PRN
+            elif self.ir == 0b01000111:
+                print(f'PRN:{operand_a}')
+                self.pc += 2
+            else:
+                self.pc += 1
