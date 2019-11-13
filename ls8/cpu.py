33200668c5397b7bf40d4db68d31f650384cfa53
christiansallen <christiansallen@gmail.com>
2019-11-12 17:00:31 -0700

    finished mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -19,33 +19,52 @@ class CPU:
     def ram_write(self, index, value):
         self.ram[index] = value
 
-    def load(self):
+    def load(self, program):
         """Load a program into memory."""
 
         address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010,  # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111,  # PRN R0
+        #     0b00000000,
+        #     0b10100010,  # MUL
+        #     0b00000001,  # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        with open(program) as f:
+            for line in f:
+                line = line.split("#")[0]
+                line = line.strip()
+                if line == '':
+                    continue
+                # print(f'Line in ls8.py: {line}')
+                val = int(line, 2)
+                print(f'val:{val}')
+                self.ram[address] = val
+                address += 1
+
+        # for instruction in program:
+
+            # print(self.ram)
+            # val = int(instruction, 2)
+            # print(val)
+            # memory[address] = val
+            # address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        # elif op == "SUB": etc
+            self.registers[reg_a] += self.registers[reg_b]
+        elif op == "MUL":
+            self.registers[reg_a] *= self.registers[reg_b]
+
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -71,6 +90,11 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
+        HALT = 0b00000001
+        LDI = 0b10000010
+        PRN = 0b01000111
+        MUL = 0b10100010
+
         self.pc = 0
         running = True
         while running is True:
@@ -78,17 +102,21 @@ class CPU:
             self.ir = self.ram[self.pc]
             operand_a = self.ram[self.pc+1]
             operand_b = self.ram[self.pc+2]
-            # HLT
-            if self.ir == 0b00000001:
+            # HLT Halt
+            if self.ir == HALT:
                 break
-            # LDI
-            elif self.ir == 0b10000010:
-                print(f'LDI:{operand_b}')
-                self.registers[operand_b-1] = operand_a
+            # LDI Store value in register  reg, val
+            elif self.ir == LDI:
+                # print(f'LDI:{operand_b}')
+                self.registers[operand_a] = operand_b
                 self.pc += 3
-            # PRN
-            elif self.ir == 0b01000111:
-                print(f'PRN:{operand_a}')
+            # PRN Print
+            elif self.ir == PRN:
+                print(f'PRN:{self.registers[operand_a]}')
                 self.pc += 2
+            # MUL multiply
+            elif self.ir == MUL:
+                self.alu("MUL", operand_a, operand_b)
+                self.pc += 3
             else:
                 self.pc += 1
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -7,5 +7,12 @@ from cpu import *
 
 cpu = CPU()
 
-cpu.load()
+if len(sys.argv) != 2:
+    print("usage: ls8.py filename")
+    sys.exit(1)
+progname = sys.argv[1]
+address = 0
+
+
+cpu.load(progname)
 cpu.run()
